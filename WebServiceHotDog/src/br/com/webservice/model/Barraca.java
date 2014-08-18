7536e0f4d95bc1c64bea747a4ef1fea170a45f2e
Kayo <lima.kayo@gmail.com>
2014-08-18 17:05:42 -0300

    Adicionado o atributo de latitude e longitude

--- WebServiceHotDog/src/br/com/webservice/model/Barraca.java
+++ WebServiceHotDog/src/br/com/webservice/model/Barraca.java
@@ -7,6 +7,21 @@ public class Barraca {
 	
 	private int id;
 	private String nome;
+	private double latitude;
+	private double longitude;
+	
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
 	public int getId() {
 		return id;
 	}
